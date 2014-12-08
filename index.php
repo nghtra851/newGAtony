f77c8e610877c4abd1ef2cfa5c6943619486e56b
viktja710 <viktja710@E29632.edu.linkoping.se>
2014-12-08 09:44:06 +0100

    herrow

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if (isset($_POST["action"]) === "signout") {
+if (isset($_POST["action"]) == "signout") {
     if (isset($_SESSION["user"]) != NULL) {
         session_unset();
         session_destroy();
