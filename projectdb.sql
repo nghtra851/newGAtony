8536f8b3d48dbbd43f58c87b5b22530d2c5903ae
Viktor Rotting Tjädermo <viktor.4@hotmail.com>
2015-02-28 17:40:19 +0100

    går ejh längre lägga till mer produkter än databas, delete bredvid?

--- projectdb.sql
+++ projectdb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.9
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
--- Värd: 127.0.0.1
--- Skapad: 01 dec 2014 kl 10:25
--- Serverversion: 5.6.14
--- PHP-version: 5.5.6
+-- Host: 127.0.0.1
+-- Generation Time: Feb 28, 2015 at 05:38 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,42 +17,69 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Databas: `projectdb`
+-- Database: `projectdb`
 --
-CREATE DATABASE IF NOT EXISTS `projectdb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `projectdb`;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `login`
+-- Table structure for table `login`
 --
 
-DROP TABLE IF EXISTS `login`;
 CREATE TABLE IF NOT EXISTS `login` (
   `username` varchar(25) NOT NULL,
-  `password` varchar(25) NOT NULL,
-  PRIMARY KEY (`username`)
+  `password` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `products`
+-- Table structure for table `products`
 --
 
-DROP TABLE IF EXISTS `products`;
 CREATE TABLE IF NOT EXISTS `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `name` varchar(25) NOT NULL,
   `price` int(10) NOT NULL,
   `color` varchar(25) NOT NULL,
-  `size` int(10) NOT NULL,
+  `size` varchar(10) NOT NULL,
   `quantity` int(10) NOT NULL,
-  `availability` int(2) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `availability` int(2) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `color`, `size`, `quantity`, `availability`) VALUES
+(1, 'Tophat', 499, 'Green, Blue, Yellow', 'S, M, L', 13, 0),
+(2, 'T-Shirt', 200, 'Brown, Blue, Orange', 'XS, M, XL', 30, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+ ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
