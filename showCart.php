75c34c610e3d48bb5c2546b9c7863eb5483b71a4
Balbin <albinsjostrand22@gmail.com>
2015-03-02 20:22:56 +0100

    gjort en räknare!

--- showCart.php
+++ showCart.php
@@ -3,13 +3,25 @@
 if (!isset($_SESSION["cart"])) {
     $_SESSION["cart"] = array();
 }
-echo "<p class='item'>Cart: <br>";
-foreach ($_SESSION["cart"] as $cart) {
-    foreach ($cart as $item) {
-        $item = ucfirst($item);
-        echo " ";
-        echo $item;
+if ($_SESSION["cart"] != NULL) {
+    echo "<p class='item'>Cart: <br>";
+    foreach ($_SESSION["cart"] as $cart) {
+        echo $cart["name"] . " ";
+        echo $cart["color"] . " ";
+        echo $cart["size"] . " ";
+        echo $cart["price"] . " £ ";
+        echo $cart["amount"] . " <br><br>";
     }
-    echo "<br>";
-}
-echo "</p>";
+    echo "</p>";
+
+    function countTotal() {
+        $total = 0;
+        foreach ($_SESSION['cart'] as $item) {
+            $total += $item['price'] * $item['amount'];
+        }
+
+        return $total;
+    }
+
+    echo "<p>Total:  " . countTotal() . "   £</p>";
+} 
